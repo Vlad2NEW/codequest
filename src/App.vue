b29e5964e21c2a8b581f6e4a6f19e2512e599b37
Vlad2NEW <149104771+Vlad2NEW@users.noreply.github.com>
2026-08-05 16:34:24 +0300

    feature: add basic navigation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <div>
+    <nav>
+      <router-link to="'/'">Головна</router-link>
+
+      |
+
+      <router-link to="'/courses'">Курси</router-link>
+
+      |
+      
+      <router-link to="'/profile'">Профіль</router-link>
+    </nav>
+
+    <hr>
+
+    <router-view />
+  </div>
 </template>
