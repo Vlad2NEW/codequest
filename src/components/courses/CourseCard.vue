079a8b2009f03ba61d833b905da9a67046846159
Vlad2NEW <149104771+Vlad2NEW@users.noreply.github.com>
2026-08-06 01:38:29 +0300

    feat: create courses list

---
+++ src/components/courses/CourseCard.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+    course: Object
+})
+</script>
+
+<template>
+    <div class="card">
+        <h3>{{ course.title }}</h3>
+        <p>{{ course.level }}</p>
+        <div>Progress: {{ course.progress }}%</div>
+
+    </div>
+</template>
+
+<style scoped>
+.card {
+    padding: 20px;
+
+    border: 1px solid #ddd;
+    border-radius: 12px;
+
+    width: 250px;
+}
+</style>
--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -1,12 +1,36 @@
 <template>
-    <div>
-        <h1>Курси</h1>
-        <p>Наші курси</p>
+    <h1>Курси</h1>
+
+    
+    <div class="courses">
+        <CourseCard v-for="course in courses" :key="course.id" :course="course" />
     </div>
+
 </template>
 
-<script>
-export default{
-    name: "CoursesPage"
-};
+<script setup>
+import CourseCard from '../components/courses/CourseCard.vue'
+
+const courses = [
+    {
+        id: 1,
+        title: "Vue.js",
+        level: "Beginner",
+        progress: 40
+    },
+    {
+        id: 2,
+        title: "JavaScript",
+        level: "Advanced",
+        progress: 70
+    }
+]
 </script>
+
+<style scoped>
+.courses {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+}
+</style>
