a8b6290b02037f0bef56c59f0f6f7c433be833e3
Vlad2NEW <149104771+Vlad2NEW@users.noreply.github.com>
2026-08-06 02:58:48 +0300

    feat: add course search

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -1,18 +1,31 @@
 <template>
     <h1>Курси</h1>
 
-    
-    <div class="courses">
-        <CourseCard v-for="course in courses" :key="course.id" :course="course" />
+
+    <input type="text" v-model="search" placeholder="Пошук курсу..." class="course-search">
+    <div v-if="filteredCourses.length" class="courses">
+        <CourseCard v-for="course in filteredCourses" :key="course.id" :course="course" />
     </div>
+
+    <p v-else>
+        Курси не знайдено
+    </p>
 
 </template>
 
 <script setup>
 import CourseCard from '../components/courses/CourseCard.vue'
 import { getCourses } from '../services/courses'
+import { ref, computed } from 'vue'
 
 const courses = getCourses()
+const search = ref('')
+
+const filteredCourses = computed(() => {
+    return courses.filter(course =>
+        course.title.toLowerCase().includes(search.value.toLowerCase())
+    )
+})
 </script>
 
 <style scoped>
@@ -20,5 +33,20 @@ const courses = getCourses()
     display: flex;
     flex-wrap: wrap;
     gap: 20px;
+}
+
+.course-search {
+    padding: 10px 15px;
+    width: 300px;
+
+    border: 1px solid #ddd;
+    border-radius: 8px;
+
+    margin-bottom: 30px;
+}
+
+.course-search:focus {
+    outline: none;
+    border-color: #333;
 }
 </style>
