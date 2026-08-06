bcfb9534ab90b33aa762db19a6a88bf622b6f5db
Vlad2NEW <149104771+Vlad2NEW@users.noreply.github.com>
2026-08-06 03:02:42 +0300

    chore: add pinia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router/index.js'
+import { createPinia } from 'pinia'
 
-createApp(App).use(router).mount('#app')
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(createPinia())
+
+app.mount('#app')
