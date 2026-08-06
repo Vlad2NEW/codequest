ebbae49cfbf915540fc309f11b85b453d4134103
Vlad2NEW <149104771+Vlad2NEW@users.noreply.github.com>
2026-08-06 17:19:11 +0300

    feat: add course level filter

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -3,6 +3,14 @@
 
 
     <input type="text" v-model="search" placeholder="Пошук курсу..." class="course-search">
+
+    <div class="filters">
+        <button v-for="level in levels" :key="level" @click="selectedLevel = level"
+            :class="{ active: selectedLevel === level }">
+            {{ level }}
+        </button>
+    </div>
+
     <div v-if="filteredCourses.length" class="courses">
         <CourseCard v-for="course in filteredCourses" :key="course.id" :course="course" />
     </div>
@@ -25,11 +33,27 @@ const courseStore = useCourseStore()
 const { courses } = storeToRefs(courseStore)
 
 const search = ref('')
+const selectedLevel = ref('All')
+const levels = [
+    'All',
+    'Beginner',
+    'Advanced'
+]
 
 const filteredCourses = computed(() => {
-    return courses.value.filter(course =>
-        course.title.toLowerCase().includes(search.value.toLowerCase())
-    )
+    return courses.value.filter(course => {
+
+        const matchesSearch =
+            course.title
+                .toLowerCase()
+                .includes(search.value.toLowerCase())
+
+        const matchesLevel =
+            selectedLevel.value === 'All' ||
+            course.level === selectedLevel.value
+
+        return matchesSearch && matchesLevel
+    })
 })
 </script>
 
@@ -53,5 +77,26 @@ const filteredCourses = computed(() => {
 .course-search:focus {
     outline: none;
     border-color: #333;
+}
+
+.filters {
+    display: flex;
+    gap: 10px;
+
+    margin-bottom: 30px;
+}
+
+.filters button {
+    padding: 8px 15px;
+
+    border: 1px solid #ddd;
+    border-radius: 8px;
+
+    cursor: pointer;
+}
+
+.filters .active {
+    background: #333;
+    color: white;
 }
 </style>
